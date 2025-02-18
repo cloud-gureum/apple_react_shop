a99c65f4717183530a393bd89abb9a07f475b3b5
cloud <lilpark1024@gmail.com>
2025-02-18 16:31:11 +0900

    Day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
@@ -7,7 +8,17 @@ function App() {
 
   return (
     <div className="App">
-
+      <Button variant="primary">Primary</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
